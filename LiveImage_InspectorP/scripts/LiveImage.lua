33e3308b2a32ecf6aae33372cc42fa2c8bc36470
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:39:26 +0200

    'Update'

--- LiveImage_InspectorP/scripts/LiveImage.lua
+++ LiveImage_InspectorP/scripts/LiveImage.lua
@@ -1,21 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  LiveImage_InspectorP
-
-  Summary:
-  Setting up camera and taking live images continuously.
-
-  How to Run:
-  A connected InpspectorP6 device is necessary to run this sample. Starting this
-  sample is possible either by running the app (F5) or debugging (F7+F10).
-  Set a breakpoint on the first row inside the main or processImage function to debug step-by-step.
-  See the acquired images in the image viewer on the DevicePage.
-
-  More Information:
-  See the tutorial "Devices - InspectorP - TriggeringAndAcquisition".
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 -- Create and configure camera
